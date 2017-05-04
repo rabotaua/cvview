2eff6d45bd94d5c54683745bd71629dcdc2438cb
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-05-04 12:44:18 +0300

    similar ui preps

--- src/components/SimilarResumes/SimilarResumesList.js
+++ src/components/SimilarResumes/SimilarResumesList.js
@@ -3,9 +3,8 @@ import { SimilarResumesListItem } from './SimilarResumesListItem'
 
 export const SimiliarResumesList = ({similar = [], getResumeText}) => {
 	if (!similar.length) return <div>nothing here</div>
-
 	return <fieldset>
 		<legend>Similar</legend>
-		{similar.map(resume => <SimilarResumesListItem key={resume.id} {...resume} getResumeText={getResumeText}/>)}
+		{similar.map(resume => <SimilarResumesListItem key={resume.id} resume={resume} getResumeText={getResumeText}/>)}
 	</fieldset>
 }
--- src/components/SimilarResumes/SimilarResumesListItem.js
+++ src/components/SimilarResumes/SimilarResumesListItem.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import ResumePhoto from '../Resume/ResumePhoto'
 
-export const SimilarResumesListItem = ({id, personal, position, getResumeText}) => <div>
-	<div>
-		<a href="#" onClick={event => getResumeText(id)}>{position.position}</a>
-		<span>{personal.name}</span>
-		<span>{personal.surName}</span>
+export const SimilarResumesListItem = ({resume, getResumeText}) => {
+	const {personal, position} = resume
+	const {name, surName, cityId, dateBirth} = personal
+	const {salary} = position
+	return <div>
+		<div>
+			<table>
+				<tbody>
+				<tr>
+					<td>
+						<ResumePhoto resume={resume} getResumeText={getResumeText}/>
+					</td>
+					<td>
+						<p>
+							<a href="#" onClick={event => getResumeText(resume.id)}>{resume.position.position}</a>
+						</p>
+						<p>
+							<span>{name} {surName}</span>
+							<span>{cityId}</span>
+							<span>{dateBirth}</span>
+							<span>{salary}</span>
+						</p>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
+}
