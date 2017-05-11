7cc1506c8a8f2816eb2df27edf9444b5bc3bea42
Anastasiya Lozhachevskaya <stesha1990@mail.ru>
2017-05-11 15:03:56 +0300

    Resume Pagination added

--- src/components/Resume/ResumePagination.js
+++ src/components/Resume/ResumePagination.js
@@ -2,9 +2,7 @@ import React from 'react'
 import scrollTo from '../../util/smoothScroll'
 
 export const ResumePagination = ({resume, getResumeText}) => {
-	// const {personal, position, experiences, updateDate} = resume
-	// const {prevID, nextID} = resume
-	console.log(resume)
+	const {prevID, nextID} = resume
 
 	const getResume = id => {
 		getResumeText(id)
@@ -12,8 +10,8 @@ export const ResumePagination = ({resume, getResumeText}) => {
 	}
 
 	return <div className="fd-f-left-middle fd-f-g20">
-		<button onClick={() => getResume(resume.id)}>prev</button>
-		<button onClick={() => getResume(resume.id)}>next</button>
+		<button onClick={() => getResume(prevID)}>prev</button>
+		<button onClick={() => getResume(nextID)}>next</button>
 	</div>
 }
 
