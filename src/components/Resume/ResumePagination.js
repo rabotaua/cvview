8d3a588e7a47085a59eaefb0f73b0a773f10b2c3
Anastasiya Lozhachevskaya <stesha1990@mail.ru>
2017-05-11 15:35:28 +0300

    resume pagination fixes

--- src/components/Resume/ResumePagination.js
+++ src/components/Resume/ResumePagination.js
@@ -5,13 +5,25 @@ export const ResumePagination = ({resume, getResumeText}) => {
 	const {prevID, nextID} = resume
 
 	const getResume = id => {
-		getResumeText(id)
+		if(id) {
+			getResumeText(id)
+		}
 		scrollTo(document.body, 0, 300)
 	}
 
-	return <div className="fd-f-left-middle fd-f-g20">
-		<button onClick={() => getResume(prevID)}>prev</button>
-		<button onClick={() => getResume(nextID)}>next</button>
+	console.log(resume)
+
+	return <div className="fd-f-right-middle fd-f-g20">
+		<span style={{cursor: 'pointer', color: '#3366cc'}} onClick={() => getResume(prevID)}>
+			<i className="fi-arrows-left"></i>
+			&nbsp;&nbsp;
+			prev
+		</span>
+		<span style={{cursor: 'pointer', color: '#3366cc'}} onClick={() => getResume(nextID)}>
+			next
+			&nbsp;&nbsp;
+			<i className="fi-arrows-right"></i>
+		</span>
 	</div>
 }
 
