8f25396d039e0feffb64ee422bb832d9ee7b6df9
RABOTA\VitaliyM <meldtown@gmail.com>
2017-05-15 16:34:00 +0300

    Displaying the list of control questions

--- src/components/ControlQuestion/ControlQuestionList.js
+++ src/components/ControlQuestion/ControlQuestionList.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
 export default class ControlQuestionList extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			isExpanded: false
+		}
+		this.toggleList = this.toggleList.bind(this)
+	}
+
+	toggleList() {
+		this.setState({
+			isExpanded: !this.state.isExpanded
+		})
+	}
+
 	render() {
+		const controlQuestions = this.props.controlQuestions ? this.props.controlQuestions.map(item => {
+			let {id, question, answer, isAnswered} = item
+			let answerText = isAnswered ? answer : 'Не уазано'
+			return <li key={id}>
+				<strong>{question}</strong><br />
+				<span style={{color: 'green'}}>{answerText}</span>
+			</li>
+		}) : []
+
 		return <div>
-			<h3>Control questions list</h3>
+			<h3 onClick={this.toggleList} style={{cursor: 'pointer'}}>
+				<b>Ответы на контрольные вопросы</b>
+			</h3>
+			<ul style={{display: this.state.isExpanded ? 'block' : 'none', listStyle: 'none', paddingLeft: 0}}>
+				{controlQuestions}
+			</ul>
 		</div>
 	}
 }
--- src/containers/ControlQuestionContainer.js
+++ src/containers/ControlQuestionContainer.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
+import {getResumeText} from '../actions/resumeActions'
 import ControlQuestionList from '../components/ControlQuestion/ControlQuestionList';
 
 class ControlQuestionContainer extends React.Component {
+	componentWillMount() {
+
+	}
+
 	render() {
 		return <div>
 			<h2>Control question container</h2>
-			<ControlQuestionList/>
+			<ControlQuestionList controlQuestions={this.props.controlQuestions}/>
 		</div>
 	}
 }
 
 const mapStateToProps = state => ({
-
+	controlQuestions: state.resume.controlQuestions
 })
 
 const mapDispatchToPops = dispatch => ({
-
+	getResumeTextAction: bindActionCreators(getResumeText, dispatch)
 })
 
 export default connect(mapStateToProps, mapDispatchToPops)(ControlQuestionContainer)
